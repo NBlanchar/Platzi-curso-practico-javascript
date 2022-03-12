2b5e888dc9e7df3b489c32921697a0e82df66720
nblanchar <nblancha1@cuc.edu.co>
2022-03-12 07:11:20 -0500

    Estadistica

--- descuentos.js
+++ descuentos.js
@@ -1,8 +1,28 @@
 //const precioOriginal = 120;
 //const descuento = 18;
 
+function Cupon(codigo,descuento){
+    this.codigo = codigo
+    this.descuento = descuento
+}
+var cupon1 = new Cupon ("baaa",15);
+var cupon2 = new Cupon ("abaa",25);
+var cupon3 = new Cupon ("aaba",55);
+var cupon4 = new Cupon ("aaab",75);
 
+var cupones = [cupon1,cupon2,cupon3,cupon4];
 
+function calcularDescuetoCoupon(precioOriginal,cupon){
+   for(let i=0;i<cupones.length;i++){
+       if(cupones[i].codigo==cupon){
+            const descuento = cupones[i].descuento
+            const porcentajeDescuento = 100 - descuento;
+            const precioConDescuento = (precioOriginal * porcentajeDescuento) / 100;
+            return "El precio con descuento es: "+precioConDescuento;
+       }
+   }
+        return "Por favor verificar el codigo del cupo "+ precioOriginal   
+}
 
 function calcularPrecioConDescuento(precioOriginal,descuento){
     const porcentajeDescuento = 100 - descuento;
@@ -20,4 +40,16 @@ function onClickButtonPriceDiscount(){
 
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento es $" +precioConDescuento;
+}
+
+function onClickButtonPriceCoupon(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+    const InputCoupon = document.getElementById("InputCoupon");
+    const CouponValue = InputCoupon.value;
+
+    const precioConDescuento = calcularDescuetoCoupon(priceValue,CouponValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = precioConDescuento;
 }
--- estadistica.js
+++ estadistica.js
@@ -1,25 +1,81 @@
-const lista1 = [
-    100, 200, 300, 500,
-];
 
-let sumaLista1 = 0;
-for (let i=0; i< lista1.length; i++) {
-    sumaLista1 = sumaLista1 + lista1[i];
+var array= []
+
+function agregarNumero(numero){
+    array.push(numero)
+    return array
 }
-const promedioLista1 = sumaLista1/lista1.length;
+
 
 function calcularPromedio(lista){
-    /*let sumaLista = 0;
+    let sumaLista = 0;
     for (let i=0; i< lista.length; i++) {
         sumaLista = sumaLista + lista[i];
-    }*/
-    const sumaLista = lista.reduce(
-        function(ValorAcumulado = 0, nuevoElemento){
-            return ValorAcumulado + nuevoElemento;
-        }
-    );
-
-
+    }
     const promedioLista = sumaLista/lista.length;  
     return promedioLista;
+}
+
+function calcularMedia(lista){
+    lista.sort(function(a,b){
+        return a-b
+    })
+    let mitad = parseInt(lista.length / 2)
+    let media; 
+    if(lista.length % 2 ===0 ){
+        let elemento1= lista[mitad-1]
+        let elemento2= lista[mitad]
+        media = (elemento1 + elemento2)/2
+    }else{
+        media = lista[mitad]
+    }
+    return media
+}
+
+function calcularModa(lista){
+    let listacount = {}
+    lista.map(
+        function(elemento){
+            if(listacount[elemento]){
+               listacount[elemento] +=1 
+            }else{
+                listacount[elemento] = 1
+            }
+        }
+    )
+    let listaArray =Object.entries(listacount).sort(
+        function (a,b){
+            return a[1]-b[1]
+        }
+    )
+    let moda = listaArray[listaArray.length-1]
+    return moda
+}
+
+function onClickButtonAddNumber(){
+    const inputNumber = document.getElementById("inputNumber");
+    const numberValue = Number(inputNumber.value);
+
+    const precioConDescuento = agregarNumero(numberValue);
+
+    const resultP = document.getElementById("lista");
+    resultP.innerText = "Los elementos en la lista son: \n" +precioConDescuento;
+}
+
+function onClickButtonCalculateAverage(){
+    const promedio = calcularPromedio(array) 
+    const resultP = document.getElementById("ResultIP");
+    resultP.innerText = "El promedio de la lista es: " +promedio;
+}
+
+function onClickButtonCalculateMedian(){
+    const media= calcularMedia(array) 
+    const resultP = document.getElementById("ResultIP");
+    resultP.innerText = "La media de la lista es: " +media;
+}
+
+function onClickButtonCalculateMode(){
+    const moda = calcularModa(array) 
+    const resultP = document.getElementById("ResultIP");
+    resultP.innerText = "La moda de la lista es: " +moda;
 }
